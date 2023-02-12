05b39a10bac4c0f1e0ecfc5fc66402fe157beec3
Sam Craig <samfcraig@gmail.com>
2023-02-12 13:10:46 -0500

    added style to footer, likely placeholder

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 import { FaGithub, FaLinkedin, FaInstagramSquare } from "react-icons/fa";
 
+const styles = {
+    footer: {
+      margin: "auto",
+      text: "black",
+      background: "gray",
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+    },
+    a: {
+      fontSize: 58,
+      margin: 2,
+    },
+  };
+
 export default function Footer() {
     return (
       <footer style={styles.footer}>
