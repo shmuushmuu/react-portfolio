ba62a52e9ba7bb7772a67c407a2de0485da6bd46
Sam Craig <samfcraig@gmail.com>
2023-02-11 19:49:31 -0500

    added forms. changed file names to .jsx. Fleshed out App.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Hi! I'm Sam, aka shmuushmuu. I'm a developer, a singer-songwriter, a gamer, an enjoyer of Dungeons and Dragons, a husband, and a foodie with IBS.
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Form from '../components/Form/Form';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>
+        E-mail: samfcraig@gmail.com
+        gitHub: shmuushmuu
+        Instagram: shmuushmuu
+      </p>
+      <Form></Form>
+    </div>
+  );
+}
+
