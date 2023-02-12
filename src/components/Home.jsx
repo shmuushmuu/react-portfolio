6cac8124892a6070c44eba34514cca2cb51304cd
Sam Craig <samfcraig@gmail.com>
2023-02-12 16:42:30 -0500

    fille dup home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,3 +5,20 @@ import pageComponents from "./pages";
 
 const pages = Object.keys(pageComponents);
 
+export default function Homepage() {
+    const [currentPage, setCurrentPage] = useState("About");
+
+    const Page = () => pageComponents[currentPage]();
+    const handlePageChange = (page) => setCurrentPage(page);
+    return (
+      <>
+        <Header
+          handlePageChange={handlePageChange}
+          pages={pages}
+          currentPage={currentPage}
+        />
+        <Page className="m-3 p-2 d-flex container min-vh-100" />
+        <Footer />
+      </>
+    );
+}
