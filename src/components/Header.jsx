17f029b93c7dc5a07bc17f7b0feaa2089c71d556
Sam Craig <samfcraig@gmail.com>
2023-02-12 16:57:11 -0500

    trouble with css. deleted unnecessary and empty css files, trying to run

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Navigation from "./Nav";
+
+export default function Header({ handlePageChange, pages, currentPage }) {
+  return (
+    <>
+      <header className="header">
+        <h1 className="name text-center">Sam Craig (shmuushmuu)</h1>
+        <Navigation
+          currentPage={currentPage}
+          handlePageChange={handlePageChange}
+          pages={pages}
+        />
+      </header>
+    </>
+  );
+}
