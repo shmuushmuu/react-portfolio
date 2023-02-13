1ef375872aed6043ce790bac0d5001514bcf8413
Sam Craig <samfcraig@gmail.com>
2023-02-12 19:21:53 -0500

    added 3 to the portfolio

--- src/components/pages/Portolio.jsx
+++ src/components/pages/Portolio.jsx
@@ -1,7 +1,43 @@
 import React, { useState } from "react";
 import Projects from "../Projects/Projects";
 import battleJots from "../../assets/visuals/battle-jots-demo.png";
-import socialNetwork from "../../assets/visuals/NoSQL-SNA-code-demo.png";
-import eCommerce from "../../assets/visuals/orm-e-commerce-back-end-screenshot.png";
+import pokemonInfoDump from "../../assets/visuals/pokemon-info-dump.png";
 import noteTaker from "../../assets/visuals/note-taker-app-screenshot.png";
-import employeeTracker from "../../assets/visuals/sql-employeetracker-updatedscreenshot.png";
+
+export default function Portfolio() {
+    const [apps, setApps] = useState(
+        [
+            {
+                title: "Battle Jots",
+                body: "Play competitive wordle with your friends!",
+                img: battleJots,
+                deployed: "https://battle-jots.herokuapp.com/",
+                gitLink: "https://github.com/shmuushmuu/battle-jots",
+                id: 1
+            },
+            {
+                title: "Pokemon Info Dump",
+                body: "Learn about Pokemon! Find a related YouTube video and info from the Pokemon API, as well as resources regarding the Pokemon types.",
+                img: pokemonInfoDump,
+                deployed: "https://shmuushmuu.github.io/Pokemon-info-dump/",
+                gitLink: "https://github.com/shmuushmuu/Pokemon-info-dump",
+                id: 2
+            },
+            {
+                title: "Note Taker App",
+                body: "Take notes that persist, and delete them with ease! A great way to keep info together just by logging into a website.",
+                img: noteTaker,
+                deployed: "https://shmuushmuu-note-taker-app.herokuapp.com/Links to an external site.",
+                gitLink: "https://github.com/shmuushmuu/note-taker-app",
+                id: 3,
+            },
+        ],
+        []
+    );
+    return (
+        <div className="m-2 min-vh-100">
+            <h1 className="text-center p-2"> Portfolio </h1>
+            <Projects apps={apps} heading="Projects" />
+        </div>
+    );
+}
